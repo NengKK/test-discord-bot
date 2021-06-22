943104eac2eabe38cd25da54091be853de7ba656
Kittisak Khamdaeng <kittisak.khamdaeng@gmail.com>
2021-06-23 01:41:06 +0700

    Test echo

--- src/bot.js
+++ src/bot.js
@@ -3,4 +3,20 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 const client = new Client();
 
+client.on('ready', () => {
+  console.log(`${client.user.username} is ready!!`);
+});
+
+client.on('message', (message) => {
+  let sender = message.author;
+  console.log(`Get message from user: ${sender.username}`);
+  
+  if (sender.id !== client.user.id) {
+    let channel = message.channel;
+    channel.send(message.content)
+      .then((message) => console.log(`Sent message back: ${message.content}`))
+      .catch((ex) => console.error(ex));
+  }
+});
+
 client.login(process.env.DISCORDJS_BOT_TOKEN);
